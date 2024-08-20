11f7e51b87e665cf34ea4ab95dea436ba9614e57
Marc <marctotaro94@gmail.com>
2024-08-19 22:14:21 -0500

    level 3

--- addons/scripts/main_menu.gd
+++ addons/scripts/main_menu.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 @onready var audio_stream_player: AudioStreamPlayer = $Control/VBoxContainer/Button/ClickSFX
 @onready var hover_sfx: AudioStreamPlayer = $Control/VBoxContainer/Button/HoverSFX
 
-
+@export var level_to_load: PackedScene
 
 
 func _on_button_button_down() -> void:
@@ -11,7 +11,7 @@ func _on_button_button_down() -> void:
 
 
 func _on_audio_stream_player_finished() -> void:
-	get_tree().change_scene_to_file("res://addons/scenes/Level.tscn")
+	get_tree().change_scene_to_packed(level_to_load)
 
 
 func _on_button_mouse_entered() -> void:
